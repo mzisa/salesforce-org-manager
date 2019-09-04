49cdbbd5b7c47ebdd5e8f946ec9ec348f9fa8848
Marco Z <marco.zisa@gmail.com>
2019-09-04 22:18:31 +0200

    release 1.7.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.7.5] - 2019-08-31
+### Changed
+- Upgraded dlls for api version 46
+
 ## [1.7.4] - 2019-08-31
 ### Changed
 - Added support for api version 46
--- SalesforceOrgManager/SalesforceOrgManager/View/Principale.Designer.cs
+++ SalesforceOrgManager/SalesforceOrgManager/View/Principale.Designer.cs
@@ -56,10 +56,9 @@
             this.picNewProject.Cursor = System.Windows.Forms.Cursors.Hand;
             this.picNewProject.Image = ((System.Drawing.Image)(resources.GetObject("picNewProject.Image")));
             this.picNewProject.ImageLocation = "";
-            this.picNewProject.Location = new System.Drawing.Point(31, 5);
-            this.picNewProject.Margin = new System.Windows.Forms.Padding(2);
+            this.picNewProject.Location = new System.Drawing.Point(46, 8);
             this.picNewProject.Name = "picNewProject";
-            this.picNewProject.Size = new System.Drawing.Size(116, 108);
+            this.picNewProject.Size = new System.Drawing.Size(174, 166);
             this.picNewProject.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picNewProject.TabIndex = 0;
             this.picNewProject.TabStop = false;
@@ -70,10 +69,9 @@
             this.picOpenProject.Cursor = System.Windows.Forms.Cursors.Hand;
             this.picOpenProject.Image = ((System.Drawing.Image)(resources.GetObject("picOpenProject.Image")));
             this.picOpenProject.ImageLocation = "";
-            this.picOpenProject.Location = new System.Drawing.Point(30, 5);
-            this.picOpenProject.Margin = new System.Windows.Forms.Padding(2);
+            this.picOpenProject.Location = new System.Drawing.Point(45, 8);
             this.picOpenProject.Name = "picOpenProject";
-            this.picOpenProject.Size = new System.Drawing.Size(116, 108);
+            this.picOpenProject.Size = new System.Drawing.Size(174, 166);
             this.picOpenProject.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picOpenProject.TabIndex = 2;
             this.picOpenProject.TabStop = false;
@@ -83,10 +81,9 @@
             // 
             this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(28, 115);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(42, 177);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(119, 16);
+            this.label1.Size = new System.Drawing.Size(178, 25);
             this.label1.TabIndex = 3;
             this.label1.Text = "OPEN PROJECT";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -95,10 +92,9 @@
             // 
             this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(308, 133);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(462, 205);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(119, 16);
+            this.label2.Size = new System.Drawing.Size(178, 25);
             this.label2.TabIndex = 4;
             this.label2.Text = "NEW PROJECT";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -107,10 +103,9 @@
             // 
             this.label3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(578, 132);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(867, 203);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(105, 16);
+            this.label3.Size = new System.Drawing.Size(158, 25);
             this.label3.TabIndex = 6;
             this.label3.Text = "SETTINGS";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -119,10 +114,9 @@
             // 
             this.cmbWorkspace.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbWorkspace.FormattingEnabled = true;
-            this.cmbWorkspace.Location = new System.Drawing.Point(4, 184);
-            this.cmbWorkspace.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbWorkspace.Location = new System.Drawing.Point(6, 283);
             this.cmbWorkspace.Name = "cmbWorkspace";
-            this.cmbWorkspace.Size = new System.Drawing.Size(709, 33);
+            this.cmbWorkspace.Size = new System.Drawing.Size(1062, 45);
             this.cmbWorkspace.TabIndex = 0;
             this.cmbWorkspace.Text = "Select Workspace";
             this.cmbWorkspace.Visible = false;
@@ -133,11 +127,10 @@
             // 
             this.lstProject.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lstProject.FormattingEnabled = true;
-            this.lstProject.ItemHeight = 25;
-            this.lstProject.Location = new System.Drawing.Point(4, 249);
-            this.lstProject.Margin = new System.Windows.Forms.Padding(2);
+            this.lstProject.ItemHeight = 37;
+            this.lstProject.Location = new System.Drawing.Point(6, 383);
             this.lstProject.Name = "lstProject";
-            this.lstProject.Size = new System.Drawing.Size(709, 104);
+            this.lstProject.Size = new System.Drawing.Size(1062, 152);
             this.lstProject.TabIndex = 8;
             this.lstProject.Visible = false;
             // 
@@ -145,10 +138,9 @@
             // 
             this.btnOpenProject.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.btnOpenProject.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnOpenProject.Location = new System.Drawing.Point(2, 446);
-            this.btnOpenProject.Margin = new System.Windows.Forms.Padding(2);
+            this.btnOpenProject.Location = new System.Drawing.Point(3, 686);
             this.btnOpenProject.Name = "btnOpenProject";
-            this.btnOpenProject.Size = new System.Drawing.Size(710, 62);
+            this.btnOpenProject.Size = new System.Drawing.Size(1065, 95);
             this.btnOpenProject.TabIndex = 2;
             this.btnOpenProject.Text = "OPEN";
             this.btnOpenProject.UseVisualStyleBackColor = true;
@@ -160,39 +152,37 @@
             this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.picOpenProject);
-            this.panel1.Location = new System.Drawing.Point(7, 18);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Location = new System.Drawing.Point(10, 28);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(181, 145);
+            this.panel1.Size = new System.Drawing.Size(272, 223);
             this.panel1.TabIndex = 10;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.panel2.Controls.Add(this.picNewProject);
-            this.panel2.Location = new System.Drawing.Point(278, 18);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Location = new System.Drawing.Point(417, 28);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(182, 145);
+            this.panel2.Size = new System.Drawing.Size(273, 223);
             this.panel2.TabIndex = 11;
             // 
             // panel3
             // 
             this.panel3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.panel3.Controls.Add(this.picSettings);
-            this.panel3.Location = new System.Drawing.Point(548, 18);
-            this.panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3.Location = new System.Drawing.Point(822, 28);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(163, 145);
+            this.panel3.Size = new System.Drawing.Size(244, 223);
             this.panel3.TabIndex = 12;
             // 
             // picSettings
             // 
             this.picSettings.Cursor = System.Windows.Forms.Cursors.Hand;
             this.picSettings.Image = ((System.Drawing.Image)(resources.GetObject("picSettings.Image")));
-            this.picSettings.Location = new System.Drawing.Point(25, 5);
+            this.picSettings.Location = new System.Drawing.Point(38, 8);
+            this.picSettings.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.picSettings.Name = "picSettings";
-            this.picSettings.Size = new System.Drawing.Size(110, 108);
+            this.picSettings.Size = new System.Drawing.Size(165, 166);
             this.picSettings.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.picSettings.TabIndex = 0;
             this.picSettings.TabStop = false;
@@ -201,19 +191,20 @@
             // txtProjectName
             // 
             this.txtProjectName.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtProjectName.Location = new System.Drawing.Point(4, 249);
+            this.txtProjectName.Location = new System.Drawing.Point(6, 383);
+            this.txtProjectName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtProjectName.Name = "txtProjectName";
-            this.txtProjectName.Size = new System.Drawing.Size(709, 38);
+            this.txtProjectName.Size = new System.Drawing.Size(1062, 53);
             this.txtProjectName.TabIndex = 1;
             this.txtProjectName.Text = "Project name...";
             this.txtProjectName.Visible = false;
             // 
             // Principale
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.ClientSize = new System.Drawing.Size(716, 523);
+            this.ClientSize = new System.Drawing.Size(1074, 805);
             this.Controls.Add(this.txtProjectName);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnOpenProject);
@@ -224,10 +215,9 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel3);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Principale";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Salesforce Org Manager v. 1.7.4";
+            this.Text = "Salesforce Org Manager v. 1.7.5";
             this.Load += new System.EventHandler(this.Principale_Load);
             ((System.ComponentModel.ISupportInitialize)(this.picNewProject)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picOpenProject)).EndInit();
